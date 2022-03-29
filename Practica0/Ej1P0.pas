3fddf62a6c94cb9705852ee36f49158d32c771c7
FrancoSantiagoTimpanaro <88697870+FrancoSantiagoTimpanaro@users.noreply.github.com>
2022-03-29 18:38:04 -0300

    FirstCommit

---
+++ Practica0/Ej1P0.pas
@@ -0,0 +1,10 @@
+program ej1p0
+    varA: real;
+    varB: real;
+begin
+    writeln ('Ingrese el primer número:');
+    readln (varA);
+    writeln ('Ingrese el segundo número:');
+    readln (varB);
+    writeln ('Los números leidos fueron:', varB, 'y', varA);
+end.
